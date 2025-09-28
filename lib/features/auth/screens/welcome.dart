f1049fc2674055c1e3f9e8a852c7787258866369
ahmed <peter43fd@gmail.com>
2025-09-28 17:55:37 +0200

    I finished designing the welcome screen

--- lib/features/auth/screens/welcome.dart
+++ lib/features/auth/screens/welcome.dart
@@ -20,10 +20,88 @@ class Welcome extends StatelessWidget {
               const SizedBox(height: 10),
               const Text(
                 "Your gateway to seamless work management.",
-                style: TextStyle(fontSize: 16),
+                style: TextStyle(fontSize: 16, color: Colors.grey),
               ),
               const SizedBox(height: 30),
               Container(width: 280, height: 1, color: Colors.red),
+              const SizedBox(height: 40),
+              Padding(
+                padding: const EdgeInsets.symmetric(horizontal: 45),
+                child: Column(
+                  children: [
+                    ElevatedButton(
+                      style: ElevatedButton.styleFrom(
+                        minimumSize: const Size(double.infinity, 50),
+                        backgroundColor: Colors.green,
+                      ),
+                      onPressed: () {
+                        print("job===============");
+                      },
+                      child: const Text(
+                        "I am looking for a job",
+                        style: TextStyle(fontSize: 16, color: Colors.white),
+                      ),
+                    ),
+                    SizedBox(height: 20),
+                    ElevatedButton(
+                      style: ElevatedButton.styleFrom(
+                        minimumSize: const Size(double.infinity, 50),
+                        backgroundColor: Colors.cyan,
+                      ),
+                      onPressed: () {
+                        print("owner============");
+                      },
+                      child: const Text(
+                        "I am a business owner",
+                        style: TextStyle(fontSize: 16, color: Colors.white),
+                      ),
+                    ),
+                    SizedBox(height: 20),
+                    const Text(
+                      "Or you can browse as a guest",
+                      style: TextStyle(fontSize: 16, color: Colors.grey),
+                    ),
+                    SizedBox(height: 20),
+                    ElevatedButton(
+                      style: ElevatedButton.styleFrom(
+                        minimumSize: const Size(double.infinity, 50),
+                        backgroundColor: Colors.black,
+                      ),
+                      onPressed: () {
+                        print("guest==============");
+                      },
+                      child: const Text(
+                        "Continue as Guest",
+                        style: TextStyle(fontSize: 16, color: Colors.white),
+                      ),
+                    ),
+                    SizedBox(height: 20),
+                    InkWell(
+                      onTap: () {
+                        print("contact============");
+                      },
+                      child: const Text.rich(
+                        TextSpan(
+                          children: [
+                            TextSpan(
+                              text: "Do you already have an account? ",
+                              style: TextStyle(color: Colors.grey),
+                            ),
+                            TextSpan(
+                              text: "Login",
+                              style: TextStyle(
+                                fontWeight: FontWeight.bold,
+                                color: Colors.blue,
+                                decoration: TextDecoration.underline,
+                              ),
+                            ),
+                          ],
+                        ),
+                      ),
+                    ),
+                  ],
+                ),
+              ),
             ],
           ),
         ],
